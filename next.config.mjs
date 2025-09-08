459ad089f76d1c7c854eb9d564373ec8b875ac36
blessedjoe2000 <blessedjoe2000@yahoo.com>
2025-09-07 19:11:50 -0500

    board member photo updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "unitedesanorganization1.s3.us-west-1.amazonaws.com",
-      "76yw7v2l2z.ufs.sh",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "76yw7v2l2z.ufs.sh",
+        pathname: "/**",
+      },
     ],
   },
 };
