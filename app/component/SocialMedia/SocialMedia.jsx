e47a6e7ed983cce5b577e995bb75512877ffd01a
blessedjoe2000 <blessedjoe2000@yahoo.com>
2025-09-08 21:50:13 -0500

    social media links added

--- app/component/SocialMedia/SocialMedia.jsx
+++ app/component/SocialMedia/SocialMedia.jsx
@@ -5,10 +5,19 @@ import "react-social-icons/meetup";
 export default function SocialMedia() {
   return (
     <div className="my-2 flex gap-2">
-      <SocialIcon network="youtube" url="https://www.youtube.com/@UnitedEsanOrganization" />
-      <SocialIcon network="facebook" url="/" />
-      <SocialIcon network="instagram" url="/" />
-      <SocialIcon network="x" url="/" />
+      <SocialIcon
+        network="youtube"
+        url="https://www.youtube.com/@UnitedEsanOrganization"
+      />
+      <SocialIcon
+        network="facebook"
+        url="https://www.facebook.com/share/1BEM85nEr1/?mibextid=wwXIfr"
+      />
+      <SocialIcon
+        network="instagram"
+        url="https://www.instagram.com/unitedesanhouston"
+      />
+      <SocialIcon network="x" url="https://x.com/Ueohoustontx" />
     </div>
   );
 }
