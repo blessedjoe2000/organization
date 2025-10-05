2038676d1078703707f66fedcfbf53a4f89911b4
blessedjoe2000 <blessedjoe2000@yahoo.com>
2025-10-05 16:19:22 -0500

    independence popup photo removed

--- app/component/FlashAlert/FlashAlert.jsx
+++ app/component/FlashAlert/FlashAlert.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FlashAlert() {
   return (
     <div className="bg-sharp-red py-1 text-white font-semibold font-bebasNeue overflow-hidden whitespace-nowrap text-xl">
-      <p className="scrolling-text">
+      {/* <p className="scrolling-text">
         Happy 65th Independence Day, Nigeria! Today, we proudly join hands
         across generations and oceans to celebrate the strength, resilience, and
         vibrant heritage of our homeland. As Esan sons and daughters, we honor
@@ -11,11 +11,12 @@ export default function FlashAlert() {
         Houston to Esanland, our hearts beat green and white. United Esan
         Organization Houston: preserving culture, promoting unity, and
         celebrating freedom.
-      </p>
-      {/* <p className="scrolling-text">
-        Esan Community Christmas Party 2025! Scheduled for December 19, 2025. At Brenda&apos;s Reception Hall located at 13900 Beechnut St in Houston, Texas 77083.
-        Come and celebrate Christmas season with us.
       </p> */}
+      <p className="scrolling-text">
+        Esan Community Christmas Party 2025! Scheduled for December 19, 2025. At
+        Brenda&apos;s Reception Hall located at 13900 Beechnut St in Houston,
+        Texas 77083. Come and celebrate Christmas season with us.
+      </p>
     </div>
   );
 }
